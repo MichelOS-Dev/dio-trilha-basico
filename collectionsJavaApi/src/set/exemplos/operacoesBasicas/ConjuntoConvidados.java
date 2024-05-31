d2fbaac6d845ed7beb3f1ec5503e48bb01c21f4d
Michel Oliveira <micheloliveira.tisi@gmail.com>
2024-05-31 13:54:33 -0300

    Adicionando Exemplo utilizando Set com operações básicas

---
+++ collectionsJavaApi/src/set/exemplos/operacoesBasicas/ConjuntoConvidados.java
@@ -0,0 +1,67 @@
+package set.exemplos.operacoesBasicas;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+
+  private Set<Convidado> convidadosSet;
+
+  public ConjuntoConvidados() {
+    this.convidadosSet = new HashSet<>();
+  }
+
+  public void adicionarConvidado(String nome, int codigoConvite) {
+    convidadosSet.add(new Convidado(nome, codigoConvite));
+  }
+
+  public void removerConvidadoPorCodigoConvite(int codigoConvite) {
+    Convidado convidadoParaRemover = null;
+    if (!convidadosSet.isEmpty()) {
+      for (Convidado c : convidadosSet) {
+        if (c.getCodigoConvite() == codigoConvite) {
+          convidadoParaRemover = c;
+          break;
+        }
+      }
+      convidadosSet.remove(convidadoParaRemover);
+    } else {
+      throw new RuntimeException("O conjunto está vazio!");
+    }
+  }
+
+  public int contarConvidados() {
+    return convidadosSet.size();
+  }
+
+  public void exibirConvidados() {
+    if (!convidadosSet.isEmpty()) {
+      System.out.println(convidadosSet);
+    } else {
+      System.out.println("O conjunto está vazio!");
+    }
+  }
+
+  public static void main(String[] args) {
+	  
+    ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+
+    System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
+
+    conjuntoConvidados.adicionarConvidado("Raphaela", 342);
+    conjuntoConvidados.adicionarConvidado("Carlos",435);
+    conjuntoConvidados.adicionarConvidado("Jones", 543);
+    conjuntoConvidados.adicionarConvidado("David", 336);
+
+    System.out.println("Convidados no conjunto:");
+    conjuntoConvidados.exibirConvidados();
+
+    System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
+
+    conjuntoConvidados.removerConvidadoPorCodigoConvite(336);
+    System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados após a remoção");
+
+    System.out.println("Convidados no conjunto após a remoção:");
+    conjuntoConvidados.exibirConvidados();
+  }
+}
