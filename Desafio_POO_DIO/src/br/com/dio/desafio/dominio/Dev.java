7c10d2fa186f2f82a0d3a8410177bb78aee08dcd
Michel Oliveira <micheloliveira.tisi@gmail.com>
2024-06-24 18:12:21 -0300

    Criando métodos na Classe Dev

--- Desafio_POO_DIO/src/br/com/dio/desafio/dominio/Dev.java
+++ Desafio_POO_DIO/src/br/com/dio/desafio/dominio/Dev.java
@@ -2,53 +2,61 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
 	
 	private String nome;
-	private Set<Conteudo> conteudo = new LinkedHashSet<>();
-	private Set<Conteudo> conteudoConcluidos = new LinkedHashSet<>();
+	private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+	private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 	
 	public void inscreverBootcamp(Bootcamp bootcamp) {
-		
+		this.conteudosConcluidos.addAll(bootcamp.getConteudos());
+		bootcamp.getDevsIncritos().add(this);
 	}
 	
 	public void progredir() {
-		
+		Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+		if (conteudo.isPresent()) {
+			this.conteudosConcluidos.add(conteudo.get());
+			this.conteudosInscritos.remove(conteudo.get());
+		}else {
+			System.err.println("Você não está matriculado em nenhum conteúdo!");
+		}
 	}
 	
-	public void calcularTotalXp() {
-		
+	public double calcularTotalXp() {
+		return this.conteudosConcluidos.stream().mapToDouble(Conteudo::calcularXp/* Ou conteudo -> conteudo.calcularXp()*/).sum();
 	}
 
 	public String getNome() {
 		return nome;
 	}
 
-	public Set<Conteudo> getConteudo() {
-		return conteudo;
+	public Set<Conteudo> getConteudoInscritos() {
+		return conteudosInscritos;
 	}
 
 	public Set<Conteudo> getConteudoConcluidos() {
-		return conteudoConcluidos;
+		return conteudosConcluidos;
 	}
 
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
-	public void setConteudo(Set<Conteudo> conteudo) {
-		this.conteudo = conteudo;
+	public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+		this.conteudosInscritos = conteudosInscritos;
 	}
 
-	public void setConteudoConcluidos(Set<Conteudo> conteudoConcluidos) {
-		this.conteudoConcluidos = conteudoConcluidos;
+	public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+		this.conteudosConcluidos = conteudosConcluidos;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(conteudo, conteudoConcluidos, nome);
+		return Objects.hash(conteudosInscritos, conteudosConcluidos, nome);
 	}
 
 	@Override
@@ -60,7 +68,7 @@ public class Dev {
 		if (getClass() != obj.getClass())
 			return false;
 		Dev other = (Dev) obj;
-		return Objects.equals(conteudo, other.conteudo) && Objects.equals(conteudoConcluidos, other.conteudoConcluidos)
+		return Objects.equals(conteudosInscritos, other.conteudosInscritos) && Objects.equals(conteudosConcluidos, other.conteudosConcluidos)
 				&& Objects.equals(nome, other.nome);
 	}
 
